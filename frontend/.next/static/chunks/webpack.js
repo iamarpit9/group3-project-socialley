c70731cdff94a7aa6dc91dcd58a519aa9a026292
iamarpit9 <gupta98960@gmail.com>
2022-05-21 22:58:45 +0530

    changes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36c1a460fc2f026e"; }
+/******/ 		__webpack_require__.h = function() { return "3cc2b991aeb85e3b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
